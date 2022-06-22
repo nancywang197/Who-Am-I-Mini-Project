9a2450bb67a25d7a02ee0752045c9164bf3b3c1b
Nancy Wang <nancy.wang197@gmail.com>
2022-06-22 15:07:48 -0500

    Initial Commit

---
+++ Who Am I Mini Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who Am I Mini Project
+//
+//  Created by Scholar on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
